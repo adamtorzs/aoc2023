17835a988e35f39cb36afe349030e89854047065
Loylos <adam434@gmail.com>
2023-12-10 20:31:49 +0100

    Cleanup in aisle 10

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -2,39 +2,25 @@ import kotlin.math.ceil
 
 class Day10 : Day {
 
-    val nodes = ArrayList<Node10>()
+    private val nodes = HashMap<Pair<Int, Int>, Node10>()
     override fun solve1(input: String): Any {
 
-        val lines = input.lines()
-        lines.forEachIndexed { y, line ->
+        input.lines().forEachIndexed { y, line ->
             line.forEachIndexed { x, char ->
-                run {
-                    nodes.add(Node10(x, y, char))
-                }
+                nodes[x to y] = Node10(x, y, char)
             }
         }
 
-        lines.forEachIndexed { y, line ->
+        input.lines().forEachIndexed { y, line ->
             line.forEachIndexed { x, char ->
-                run {
+                nodes[x to y]?.apply {
                     when (char) {
-                        'F' -> getNode(nodes, x, y)?.addNeighbor(getNode(nodes, x, y + 1))
-                            ?.addNeighbor(getNode(nodes, x + 1, y))
-
-                        'L' -> getNode(nodes, x, y)?.addNeighbor(getNode(nodes, x, y - 1))
-                            ?.addNeighbor(getNode(nodes, x + 1, y))
-
-                        '7' -> getNode(nodes, x, y)?.addNeighbor(getNode(nodes, x, y + 1))
-                            ?.addNeighbor(getNode(nodes, x - 1, y))
-
-                        'J' -> getNode(nodes, x, y)?.addNeighbor(getNode(nodes, x, y - 1))
-                            ?.addNeighbor(getNode(nodes, x - 1, y))
-
-                        '|', 'S' -> getNode(nodes, x, y)?.addNeighbor(getNode(nodes, x, y - 1))
-                            ?.addNeighbor(getNode(nodes, x, y + 1))
-
-                        '-' -> getNode(nodes, x, y)?.addNeighbor(getNode(nodes, x - 1, y))
-                            ?.addNeighbor(getNode(nodes, x + 1, y))
+                        'F' -> this.neighbors = listOf(nodes[x to y + 1], nodes[x + 1 to y])
+                        'L' -> this.neighbors = listOf(nodes[x to y - 1], nodes[x + 1 to y])
+                        '7' -> this.neighbors = listOf(nodes[x to y + 1], nodes[x - 1 to y])
+                        'J' -> this.neighbors = listOf(nodes[x to y - 1], nodes[x - 1 to y])
+                        '|', 'S' -> this.neighbors = listOf(nodes[x to y - 1], nodes[x to y + 1])
+                        '-' -> this.neighbors = listOf(nodes[x - 1 to y], nodes[x + 1 to y])
                     }
                 }
             }
@@ -43,13 +29,13 @@ class Day10 : Day {
         val startNode = getStartNode(nodes)!!
         startNode.steps = 0
         var previousNode = startNode
-        var currentNode = startNode.neighbors[0]
+        var currentNode = startNode.neighbors[0]!!
 
         while (currentNode != startNode) {
             if (currentNode.steps == -1) currentNode.steps = previousNode.steps + 1
-            val nextNode = currentNode.neighbors.filter { it.steps <= 0 }.minBy { it.steps }
+            val nextNode = currentNode.neighbors.filter { it?.steps!! <= 0 }.minBy { it?.steps!! }
             previousNode = currentNode
-            currentNode = nextNode
+            currentNode = nextNode!!
         }
 
         return ceil(previousNode.steps / 2.0)
@@ -57,48 +43,30 @@ class Day10 : Day {
 
     override fun solve2(input: String): Any {
         markNonLoop(nodes)
-        var innerNodes = 0
-        for (node in nodes.filter { it.char == 'X' }) {
-            var inside = false
-            var s = buildString { for (i in 0..node.x) append(getNode(nodes, i, node.y)?.char) }
-            s = s.filter { "FLJ7|S".contains(it) }
-            for (i in s.indices) {
-                when (s[i]) {
-                    'F' -> inside = inside.xor(s[i + 1] == 'J')
-                    'L' -> inside = inside.xor(s[i + 1] == '7')
-                    '|', 'S' -> inside = !inside
-                }
-            }
-            if (inside) {
-                innerNodes++
+        return nodes.values.count { it.char == 'X' && isInsideLoop(it) }
+    }
+
+    private fun isInsideLoop(node: Node10): Boolean {
+        var inside = false
+        val s = buildString { (0..node.x).forEach { append(nodes[it to node.y]?.char) } }
+            .filter { "FLJ7|S".contains(it) }
+
+        s.indices.forEach {
+            when (s[it]) {
+                'F' -> inside = inside xor (s[it + 1] == 'J')
+                'L' -> inside = inside xor (s[it + 1] == '7')
+                '|', 'S' -> inside = !inside
             }
         }
 
-        return innerNodes
+        return inside
     }
 
     override fun fileName(): String = "10"
+    private fun getStartNode(map: HashMap<Pair<Int, Int>, Node10>): Node10? = map.values.find { it.char == 'S' }
 
-    private fun getNode(list: List<Node10>, x: Int, y: Int): Node10? {
-        for (node in list) {
-            if (node.x == x && node.y == y) {
-                return node
-            }
-        }
-        return null
-    }
-
-    private fun getStartNode(list: List<Node10>): Node10? {
-        for (node in list) {
-            if (node.char == 'S') {
-                return node
-            }
-        }
-        return null
-    }
-
-    private fun markNonLoop(list: List<Node10>) {
-        for (node in list) {
+    private fun markNonLoop(map: HashMap<Pair<Int, Int>, Node10>) {
+        map.forEach { (_, node) ->
             if (node.steps == -1) {
                 node.char = 'X'
             }
@@ -107,13 +75,7 @@ class Day10 : Day {
 }
 
 class Node10(val x: Int, val y: Int, var char: Char) {
-    val neighbors = ArrayList<Node10>()
+    lateinit var neighbors: List<Node10?>
     var steps = -1
 
-    fun addNeighbor(node: Node10?): Node10 {
-        if (node != null) {
-            neighbors.add(node)
-        }
-        return this
-    }
 }
